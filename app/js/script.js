b73abac570dbd04cff0a325fe3f397ee948261db
Anton Trofymchuk <antontrofymchuk@Antons-MacBook-Air.local>
2018-03-04 13:36:00 +0200

    slider ready v1

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,14 @@
-var elems = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot")
+'use strict';
+let elems = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
 
+currentSlide(1);
 
 function currentSlide(n) {
   for (let i = 0; i < elems.length; i++){
     elems[i].style.display = "none";
-
+    dots[i].style.backgroundColor = "#bbb";
   }
   elems[n - 1].style.display = "block";
-  console.log(n - 1);
+  dots[n - 1].style.backgroundColor = "#272727";
 }
